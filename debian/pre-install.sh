5ebfa0b94cea2ea8c1c65c92e7b3d0baee8196fc
Chandra Prakash Singh <cps274203@gmail.com>
2024-08-20 14:18:10 +0530

    Update pre-install.sh

--- debian/pre-install.sh
+++ debian/pre-install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 #upgrade the packages
+curl -sSL https://packages.sury.org/php/README.txt | bash -x
 apt-get update && apt-get upgrade -y
 
 #install packages
-apt-get install -y git lsb-release
+apt-get install -y git lsb-release 
 
 #get the install script
-cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
+cd /usr/src && git clone https://github.com/cps274203/fusionpbx-install.git
+chmod 755 /usr/src/fusionpbx-install/debian/resources/applications.sh
 
 #change the working directory
-cd /usr/src/fusionpbx-install.sh/debian
+cd /usr/src/fusionpbx-install/debian
